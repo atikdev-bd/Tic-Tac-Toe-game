70f01de8685ffdbd175d245bd45b412eaacd3583
“atikdev-bd” <atikdev.bd@gmail.com>
2024-01-12 14:44:49 +0600

    this is my frist commit in this tic tac toe game projects

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-red-500 text-center font-bold underline">
+       Welcome to Tic-Tac-Toe game!
+      </div>
+    </>
+  );
+}
+
+export default App;
